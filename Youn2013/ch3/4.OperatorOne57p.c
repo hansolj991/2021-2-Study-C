3f25d0817f9781a03908b3bb5fb3d2be2766163a
정한솔 <hjs8170@naver.com>
2021-09-08 23:51:07 +0900

    2021.09.08

--- Youn2013/ch3/4.OperatorOne57p.c
+++ Youn2013/ch3/4.OperatorOne57p.c
@@ -6,7 +6,7 @@ int main(void)
     printf("%d+%d=%d \n", num1, num2, num1+num2);
     printf("%d-%d=%d \n", num1, num2, num1-num2);
     printf("%d*%d=%d \n", num1, num2, num1*num2);
-    printf("%d/%d=%d의 뮧=%d \n", num1, num2, num1/num2);
+    printf("%d/%d=%d의 몫=%d \n", num1, num2, num1/num2);
     printf("%d/%d=%d의 나머지=%d \n", num1, num2, num1%num2);
     return 0;
 
