1d36e87df00b37d49c1d4a6a2af79c1398c48517
정한솔 <hjs8170@naver.com>
2021-09-09 01:36:33 +0900

    2021.09.09

---
+++ Youn2014/ch03/10.OperatorSeven.66p.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num1 = 10;
+    int num2 = 12;
+    int result1, result2, result3;
+
+    result1 = (num1 == 0 && num2 == 12); //(and 연산자)
+    result2 = (num1 < 12 || num2>12); //(or 연산자)
+    result3 = (!num1);               //(not연산자)
+
+    printf("result1: %d \n", result1);
+    printf("result2: %d \n", result2);
+    printf("result3: %d \n", result3);
+    return 0;
+}
---
+++ Youn2014/ch03/11.CommaOp.67p.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num1 = 1, num2 = 2;
+    printf("Hello "), printf("world! \n"); // 콤마 사용해서 프린트 연속호출 가능
+    num1++, num2++;
+    printf("%d", num1), printf("%d", num2), printf("\n");
+    return 0;
+}
